a82df9693323fbb909a830a152044707e358acb4
Jyoti Prakash Panigrahi <105423161+hellojyoti03@users.noreply.github.com>
2024-09-19 22:19:36 +0530

    Update index.js
    h

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const axios = require('axios');
+const cors = require("cors");
+
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 // WhatsApp API credentials
 const phoneNumberId = '436620666197107';
